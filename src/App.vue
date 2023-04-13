f3b20bfecca51f2e9408d3dc8ac56c33f000ec8c
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-13 14:26:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<!-- 
+Descrizione:
+
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+
+Bonus:
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+
+Consigli:
+- iniziate a pensare prima alla struttura tutta dentro al componente pricipale App.vue. In un secondo momento fate refactoring creando componenti.
+- Idem per il sass. Iniziate a scrivere tutto dentro app.scss o direttamente nei componenti singoli e poi fate refactoring creando parziali come visto in classe.
+- Non dimenticate di analizzare il layout con Penpot
+ -->
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped></style>
