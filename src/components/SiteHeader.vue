f2604fc1c3e25fd099ca7b6fc57a189a80552163
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-14 09:24:53 +0200

    ad colored border to navbar

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -12,18 +12,16 @@ export default {
 <template>
     <header>
         <div class="container d-flex justify-content-between text-uppercase">
-            <div class="logo">
-                <img class="ms-2" src="../assets/img/dc-logo.png" alt="">
+            <div class="logo d-flex">
+                <img class="ms-2 align-self-center" src="../assets/img/dc-logo.png" alt="">
             </div>
-            <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li v-for="item in menuList" class="nav-item">
-                                <a class="nav-link" href="#">{{ item }}</a>
-                            </li>
-                        </ul>
-                    </div>
+            <nav class="navbar navbar-expand-lg p-0">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li v-for="item in menuList" class="nav-item">
+                            <a class="nav-link py-4" href="#">{{ item }}</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </div>
